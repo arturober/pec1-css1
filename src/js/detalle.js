ec3421e6b1da9dc4d5f661eb64d4ca1e22624c90
Arturo Bernal <arturober@gmail.com>
2026-04-29 23:43:45 +0200

    Animaciones y clip path

--- src/js/detalle.js
+++ src/js/detalle.js
@@ -1,14 +1,15 @@
-import { MapBuilder } from './map-builder';
+import { MapBuilder } from "./map-builder";
 
 function showMap() {
   const mapSection = document.getElementById("map");
   const coords = {
     latitude: +mapSection.dataset.lat,
     longitude: +mapSection.dataset.lng,
-  }
+  };
 
   const mapBuilder = new MapBuilder(coords, "map", 16);
   const marker = mapBuilder.createMarker(coords);
 }
 
 showMap();
+
